fe8d22ad3582b8ead02aa3dc36c4de54cdaaf3bb
中島悠介 <s1511454@u.tsukuba.ac.jp>
2018-02-15 20:10:04 +0900

    引用番号を除去

--- src/com/Yusuke/java/WindowController.java
+++ src/com/Yusuke/java/WindowController.java
@@ -13,7 +13,15 @@ public class WindowController {
 	@FXML
 	protected void buttonAction(ActionEvent event) {
 		String str = inputText.getText();
+		
+		//改行文字を半角スペースに変換
 		str = str.replace("\n", " ");
+		
+		//引用番号を半角スペースに変換
+		str = str.replaceAll(" *\\[[0-9]+\\] *", "%%QUOTATION%%");
+		str = str.replaceAll("%%QUOTATION%%\\.", "."); //文末の引用は半角スペース不要
+		str = str.replaceAll("%%QUOTATION%%", " ");
+		
 		outputText.setText(str);
 	}
 }
